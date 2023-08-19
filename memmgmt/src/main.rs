8d80b92cb934792f007b48efe2fd3db0f2f94fc9
Miha <stele1000@gmail.com>
2023-08-19 18:51:46 +0200

    Add comments

--- memmgmt/src/main.rs
+++ memmgmt/src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
     println!("Hello, world!");
 }
+
+
+// Rust idea - one variable has one memory address or pointer
+
+// Only one variable can own a piece of memory at a time
+// For primitive types, the value is stored on the stack
+// For complex types, the value is stored on the heap
+// For primitive types, copying data is cheap because we know the size of the data at compile time
+// for complex types, ownership is transferred when we assign a value to another variable
